81268596491c5683a41af602e10afeb828efae2a
Md. Emarot Hossen <146443841+emarothossen@users.noreply.github.com>
2023-10-08 20:13:11 +0600

    Add files via upload

---
+++ Assigment 1/task 1.php
@@ -0,0 +1,41 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Personal Information</title>
+</head>
+
+<body>
+    <div class="container mt-3">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card border-primary">
+                    <div class="card-header bg-primary text-white text-center">
+                        <h2>My Personal Information</h2>
+                    </div>
+                    <div class="card-body">
+                        <ul class="list-group">
+                            <li class="list-group-item"><strong>My Name:</strong>
+                                <?php echo "{$myName}"; ?>
+                            </li>
+                            <li class="list-group-item"><strong>My Age:</strong>
+                                <?php echo "{$age}";?>
+                            </li>
+                            <li class="list-group-item"><strong>My Country:</strong>
+                                <?php echo "{$country}";?>
+                            </li>
+                            <li class="list-group-item text-justify"><strong>My Brief Introduction:</strong>
+                                <?php echo "<br/>{$brifeIntroduction}";?>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
